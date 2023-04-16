7bfd235ebc3bf8671c54a9dbb7bca49c3a96f9bc
Ngaremaina <ngaremaina4@gmail.com>
2023-04-16 14:10:09 +0300

    Completed assignment

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ const fetchData = () => {
     
 }
 
+let resultContainer = document.querySelector(".searchresults")
+let titleContainer = document.querySelector(".resultstitle")
+let bookContainer = document.querySelector(".books")
+
 //Searching data from the json server
 const getSearchData = (value) => {
     let form = document.querySelector("#search")
@@ -18,10 +22,6 @@ const getSearchData = (value) => {
 
         //Getting our value from the user 
         let searchname = document.querySelector("#searchname").value
-
-        //Quering the HTML elements from the index file
-        let parentContainer = document.querySelector(".searchresults")
-        let titleContainer = document.querySelector(".resultstitle")
 
         //Creating the paragraph HTML element dynamically
         let text = document.createElement("p")
@@ -41,244 +41,16 @@ const getSearchData = (value) => {
 
                 //Once successful, we can see our results from console
                 console.log(element[1].title)
+                
                 //Assigning the variable result to our filtered data
                 let result = element[1]
-                
-                 //Creating the HTML elements dynamically
-                let card  = document.createElement("div")
-                let container = document.createElement("div")
-                let buttons = document.createElement("div")
 
-                //Setting attributes to the elements
-                card.setAttribute("class", "card")
-                container.setAttribute("class", "container")
-                buttons.setAttribute("class", "buttons")
+                let results = createElements(result)
+                // let searching = document.createElement("div")
 
-                let title = document.createElement("p")
-                let cover = document.createElement("img")
-                let price = document.createElement("p")
-                let description = document.createElement("p")
-                let availableBooks = document.createElement("p")
-                let author = document.createElement("p")
-                let details = document.createElement("details")
-                let summary = document.createElement("summary")
-                let pages = document.createElement("p")
-                let published = document.createElement("p")
-                let publisher = document.createElement("p")
-                let detailsComment = document.createElement("details")
-                let summaryComment = document.createElement("summary")
-                let name = document.createElement("p")
-                let comments = document.createElement("p")
-                let buybook = document.createElement("button")
-                let showbook = document.createElement("a")
-                let deletebook = document.createElement("button")
-                let editbook = document.createElement("button")
-                let commentbutton = document.createElement("button")
-                
-                //Setting attributes to the buttons
-                buybook.setAttribute("id","buybook")
-                deletebook.setAttribute("id","deletebook")
-                commentbutton.setAttribute("id","commentbutton")
-            
-                //Assigning the data values to the respective HTML elements
-                title.innerText = result.title
-                cover.src = result.cover
-                price.innerText = `Price: Kshs. ${result.price}`
-                description.innerText = result.description
-                author.innerText = `by ${result.author}`
-                summary.innerText = "Description"
-                pages.innerText = `Pages: ${result.pages}`
-                published.innerText = `Publish Date: ${result.published}`
-                publisher.innerText = `Publisher: ${result.publisher}`
-                showbook.innerText = "Show More..."
-                deletebook.innerHTML = "<i class=\"fa fa-trash\" aria-hidden=\"true\"></i>"
-                editbook.innerHTML = "<i class=\"fa fa-pencil\" aria-hidden=\"true\"></i>"
-                commentbutton.innerHTML = "<i class=\"fa fa-comments\" aria-hidden=\"true\"></i>"
-                summaryComment.innerText = "Comments"
-
-                //Calculating the available copies of the books and displaying to the user
-                let diff = parseInt(result.quantity) - parseInt(result.sold)
-                availableBooks.innerText = `Available copies: ${diff}`
-                
-                
-                //Comparing the books available are less or equal to 0
-                if (diff <= 0){
-                    buybook.innerText = "Sold Out"
-                    buybook.disabled = true      
-                }
-                else{
-                    buybook.innerText = "Buy Book"
-                }
-            
-                //Appending the HTML elements to the index file
-                card.appendChild(cover)
-                container.appendChild(title)
-                container.appendChild(price)
-                container.appendChild(availableBooks)
-                container.appendChild(showbook)
-                parentContainer.appendChild(card)
-                card.append(container)
-                card.append(buybook)
-
-                //adding a click event listener to our show book link
-                showbook.addEventListener("click", () => {
-
-                    //appending all the details in the html file
-                    container.appendChild(author)
-                    container.appendChild(details)
-                    details.appendChild(summary)
-                    details.appendChild(description)
-                    container.appendChild(publisher)
-                    container.appendChild(published)
-                    container.appendChild(pages)
-                    container.appendChild(detailsComment)
-                    detailsComment.appendChild(summaryComment)
-                    detailsComment.appendChild(name)
-                    detailsComment.appendChild(comments)
-                    container.appendChild(buttons)
-                    buttons.appendChild(editbook)
-                    buttons.appendChild(commentbutton)
-                    buttons.appendChild(deletebook)   
-                
-                })
-                //adding a click event listener to our buy book button
-                buybook.addEventListener('click', () => {
-                    value.sold ++
-                    let sold = value.sold
-                    let posId = value.id
-                    updateTicketNum(posId, {sold})  //updating ticket number    
-                })
-                //adding a click event listener to our delete book button
-                deletebook.addEventListener("click", () => { 
-                    let posId = value.id
-                    deleteBook(posId)//deleting book
-                })
-
-                //adding a click event listener to our edit book button
-                editbook.addEventListener("click", () => {
-                    let posId = value.id //getting the book id
-
-                    //Creating the HTML elements and appending to the card
-                    let detailForm = document.createElement("details")
-                    let formSummary = document.createElement("summary")
-                    formSummary.innerText = "Update Book"
-                    container.appendChild(detailForm)
-                    detailForm.appendChild(formSummary)
-                    let formContainer = document.createElement("div")
-                    
-                    //Adding a form to our card that will capture the updated values
-                    formContainer.innerHTML = `
-                    <form id="updateBook" class="updateformbook"><br>
-                    <label for="title">Book Title</label>
-                    <input type="text" id="title" class="form-control" placeholder="Enter the Book Title...">
-                    <label for="author">Book Author</label>
-                    <input type="text" id="author" class="form-control" placeholder="Enter the Book Author...">
-                    <label for="price">Book Price</label>
-                    <input type="number" id="price" class="form-control" placeholder="Enter the Book Price...">
-                    <label for="quantity">Quantity</label>
-                    <input type="number" id="quantity" class="form-control" placeholder="Enter the Number of Available Books to be Sold...">
-                    <label for="sold">Books Sold</label>
-                    <input type="number" id="sold" class="form-control" placeholder="Enter the Number of Books Sold...">
-                    <label for="title">Book Description</label>
-                    <input type="text" id="description" class="form-control" placeholder="Enter the Book Description..">
-                    <label for="cover">Book Cover</label>
-                    <input type="text" id="cover" class="form-control" placeholder="Enter the Book Image Link...">
-                    <button type="submit">Submit</button>
-                </form>`
-
-                //Adding a submit event listener that will update data
-                    formContainer.addEventListener("submit", (event) =>{
-                        event.preventDefault()
-
-                        //Fetching the values from user
-                        let title = document.getElementById("title").value
-                        let subtitle = document.getElementById("title").value
-                        let author = document.getElementById("author").value
-                        let published = document.getElementById("published").value
-                        let publisher = document.getElementById("publisher").value
-                        let pages = document.getElementById("pages").value
-                        let price = parseInt(document.getElementById("price").value)
-                        let quantity = parseInt(document.getElementById("quantity").value)
-                        let sold = parseInt(document.getElementById("sold").value)
-                        let description = document.getElementById("description").value
-                        let cover = document.getElementById("cover").value
-                        //PUT function that will update the book by obtaining the id
-                        fetch(`http://localhost:3000/books/${posId}`,{
-                            method:"PUT",
-                            headers:{
-                                "Content-Type":"application/json"
-                            },
-                            body:JSON.stringify({
-                                title:title,
-                                subtitle:subtitle,
-                                author:author,
-                                published:published,
-                                publisher:publisher,
-                                pages:pages,
-                                price:price,
-                                quantity:quantity,
-                                sold:sold,
-                                description:description,
-                                cover:cover
-                            })
-                        })
-                    })
-                    detailForm.appendChild(formContainer)
-                    
-                    return formContainer
-
-                },{once : true})
-
-                //Adding a click listener for the comment button
-                commentbutton.addEventListener("click", () => {
-                    let posId = value.id//get id of book
-
-                    //Creating and appending the details and summary tag
-                    let detailForm = document.createElement("details")
-                    let formSummary = document.createElement("summary")
-                    formSummary.innerText = "Add Comment"
-                    container.appendChild(detailForm)
-                    detailForm.appendChild(formSummary)
-                    let formComment = document.createElement("div")
-                    
-                    //Creating our comment form
-                    formComment.innerHTML = `
-                    <form id="commentBook" class="updateformbook"><br>
-                    <label for="name">Name</label>
-                    <input type="text" id="name" class="form-control" placeholder="Enter Your Name...">
-                    <label for="comment">Comment</label>
-                    <input type="text" id="comment" class="form-control" placeholder="Enter Your Comment...">
-                    <button type="submit">Submit</button>
-                    
-                </form>`
-
-                //adding a submit listener to our form
-                    formComment.addEventListener("submit", (event) =>{
-                        event.preventDefault()
-
-                        //Getting our values from the user
-                        let name = document.getElementById("name").value
-                        let comment = document.getElementById("comment").value
-                        
-                        //Updating our book through PATCH method
-                        fetch(`http://localhost:3000/books/${posId}`,{
-                            method:"PATCH",
-                            headers:{
-                                "Content-Type":"application/json"
-                            },
-                            body:JSON.stringify({
-                                name:name,
-                                comment:comment,
-                                
-                            })
-                    })
-                    })
-                    detailForm.appendChild(formComment)//appending our form to the index file
-                    return formComment
-                },{once : true})
-                
-                                        
-                            
+                // titleContainer.appendChild(searching)    
+                // searching.appendChild(results)
+                resultContainer.appendChild(results)
             }
           
         })     
@@ -294,7 +66,7 @@ bookForm.addEventListener("submit", (event) => {
 
     //Getting the values from user
     let title = document.getElementById("title").value
-    let subtitle = document.getElementById("title").value
+    let subtitle = document.getElementById("subtitle").value
     let author = document.getElementById("author").value
     let published = document.getElementById("published").value
     let publisher = document.getElementById("publisher").value
@@ -336,15 +108,13 @@ const listBooks = (value)=> {
 }
 
 //Creating the HTML elements
-const createElements = (value) => {
-    //Querying the HTML containers
-    let parentContainer = document.querySelector(".books")
+const createElements = (value) => {  
+    //Creating HTML elements dynamically
     let card  = document.createElement("div")
     let container = document.createElement("div")
     let buttons = document.createElement("div")
-
-    //Creating HTML elements dynamically
     let title = document.createElement("p")
+    let subtitle = document.createElement("p")
     let cover = document.createElement("img")
     let price = document.createElement("p")
     let description = document.createElement("p")
@@ -376,6 +146,7 @@ const createElements = (value) => {
 
     //Assigning values to the HTML elements
     title.innerText = value.title
+    subtitle.innerText = value.subtitle
     cover.src = value.cover
     price.innerText = `Price: Kshs. ${value.price}`
     description.innerText = value.description
@@ -411,7 +182,7 @@ const createElements = (value) => {
     container.appendChild(price)
     container.appendChild(availableBooks)
     container.appendChild(showbook)
-    parentContainer.appendChild(card)
+    bookContainer.appendChild(card)
     card.append(container)
     card.append(buybook)
 
@@ -419,6 +190,7 @@ const createElements = (value) => {
     showbook.addEventListener("click", () => {
 
         //appending all the details in the html file
+        container.appendChild(subtitle)
         container.appendChild(author)
         container.appendChild(details)
         details.appendChild(summary)
